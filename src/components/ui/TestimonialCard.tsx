f629119c3838b2a6006e1f97e6b2a68f664b9d16
syedhamzasiff <syedhamzasiff@gmail.com>
2024-10-17 02:51:03 +0500

    fix linting error

--- src/components/ui/TestimonialCard.tsx
+++ src/components/ui/TestimonialCard.tsx
@@ -10,7 +10,7 @@ function TestimonialCard ({ text, author, rating }: TestimonialCardProps) {
   return (
     <div className="flex flex-col justify-between rounded-xl bg-white shadow-lg ring-1 ring-gray-300 transition-transform transform hover:scale-105 p-6 w-72 h-72">
       <blockquote className="text-lg leading-7 tracking-tight text-gray-700 italic flex-grow">
-        <p>"{text}"</p>
+        <p>&quot;{text}&quot;</p>
       </blockquote>
       <div className="mt-4">
         <p className="font-bold text-gray-800 text-md">{author}</p>

dbbf9c4b8bacebac7df1f0c0cf0d1261c6976767
syedhamzasiff <syedhamzasiff@gmail.com>
2024-10-17 12:28:04 +0500

    fix ui responsiveness

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -5,15 +5,15 @@ import CtaButton from '../ui/CtaButton';
 function HeroSection() {
   return (
     <section className="min-h-screen flex items-center justify-between bg-gradient-to-r from-green-200 to-green-50 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-center">
+      <div className="max-w-7xl mx-auto flex flex-col sm:flex-row items-center">
         <div className="md:w-1/2 space-y-6 text-center md:text-left">
-          <h1 className="text-4xl sm:text-5xl font-bold text-gray-800 leading-tight">Empower Your Financial Journey</h1>
+          <h1 className="text-4xl sm:text-5xl font-bold text-gray-800 leading-tight md:mt-2 mt-12">Empower Your Financial Journey</h1>
           <p className="text-gray-700 text-lg md:max-w-lg">With SaveMate, take charge of your finances and reach your goals effortlessly.</p>
           <div className="flex justify-center md:justify-start">
             <CtaButton text="Join the Movement" href="#download" />
           </div>
         </div>
-        <div className="mt-6 md:mt-0 md:w-1/2 flex justify-center">
+        <div className="sm:mt-0 md:w-1/2 flex justify-center">
           <Image 
             src='/images/hero-section-image.png' 
             alt='Hero Section Image' 
--- src/components/sections/TestimonialsSection.tsx
+++ src/components/sections/TestimonialsSection.tsx
@@ -4,7 +4,7 @@ import TestimonialSlider from '../ui/TestimonialSlider';
 function TestimonialSection() {
   return (
     <section className=" py-4 md:py-10 flex flex-col items-center justify-center h-screen bg-gradient-to-b from-white to-green-50" id="testimonials">
-      <h1 className="text-5xl  font-bold text-gray-800 mb-6 text-center mx-2 md:mx-0">
+      <h1 className="text-5xl  font-bold text-gray-800 mb-6 text-center mx-2 md:mx-0 lg:mt-10">
         Hear from Our Community
       </h1>
       <h2 className="text-lg sm:text-xl md:text-2xl text-gray-800 mb-8 text-center mx-2 md:mx-0">
